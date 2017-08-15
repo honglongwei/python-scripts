d829c91f23ad540ff3cbdd77fec3fc3c6133a50c
Zline <honglongwei@users.noreply.github.com>
2017-08-15 20:14:31 +0800

    Create unixtimeformat.py
    Unix时间戳转换

---
+++ unixtimeformat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+ 
+import time
+ 
+def timestamp_datetime(value):
+    format = '%Y-%m-%d %H:%M:%S'
+    # value为传入的值为时间戳(整形)，如：1332888820
+    value = time.localtime(value)
+    ## 经过localtime转换后变成
+    ## time.struct_time(tm_year=2012, tm_mon=3, tm_mday=28, tm_hour=6, tm_min=53, tm_sec=40, tm_wday=2, tm_yday=88, tm_isdst=0)
+    # 最后再经过strftime函数转换为正常日期格式。
+    dt = time.strftime(format, value)
+    return dt
+ 
+def datetime_timestamp(dt):
+     #dt为字符串
+     #中间过程，一般都需要将字符串转化为时间数组
+     time.strptime(dt, '%Y-%m-%d %H:%M:%S')
+     ## time.struct_time(tm_year=2012, tm_mon=3, tm_mday=28, tm_hour=6, tm_min=53, tm_sec=40, tm_wday=2, tm_yday=88, tm_isdst=-1)
+     #将"2012-03-28 06:53:40"转化为时间戳
+     s = time.mktime(time.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+     return int(s)
+ 
+if __name__ == '__main__':
+    d = datetime_timestamp('2015-07-28 21:53:40')
+    print d
+    s = timestamp_datetime(1352889820)
+    print s
